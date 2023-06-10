6039400e42381debd7d1dab807e6fb23de011666
sina <cena.pakdel@gmail.com>
2023-06-10 21:16:36 +0330

    add IODispatcher, MainDispatcher, CpuDispatcher annotation

---
+++ core/network/src/main/java/com/sina/network/annotation/Dispatchers.kt
@@ -0,0 +1,15 @@
+package com.sina.network.annotation
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IODispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MainDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class CpuDispatcher

080a8b4bceacbae8992f1033e22e738398efa085
sina <cena.pakdel@gmail.com>
2023-06-10 21:12:28 +0330

    impl services

---
+++ core/network/src/main/java/com/sina/network/services/products/ProductsService.kt
@@ -0,0 +1,24 @@
+package com.sina.network.services.products
+
+import com.sina.model.data.products_dto.ProductsDTOItem
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ProductsService {
+
+    @GET("products")
+    suspend fun getLatestProducts(
+        @Query("orderby") orderBy: String = "date",
+    ): List<ProductsDTOItem>
+
+    @GET("products")
+    suspend fun getMostVisitedProducts(
+        @Query("orderby") orderBy: String = "popularity",
+    ): List<ProductsDTOItem>
+
+    @GET("products")
+    suspend fun getTopRatedProductsService(
+        @Query("page") page: Int = 1,
+        @Query("orderby") orderBy: String = "rating",
+    ): List<ProductsDTOItem>
+}

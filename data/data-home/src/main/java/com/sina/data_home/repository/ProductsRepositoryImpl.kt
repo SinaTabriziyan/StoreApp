2864d79136f271ab800d5b64ea28a31b291c3057
sina <cena.pakdel@gmail.com>
2023-06-10 21:45:12 +0330

    impl productRepository

---
+++ data/data-home/src/main/java/com/sina/data_home/repository/ProductsRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.sina.data_home.repository
+
+import com.sina.data_home.remote.ProductsRemoteDataSource
+import com.sina.domain_main.repository.ProductsRepository
+import com.sina.model.ui.products_item.ProductsItem
+import com.sina.network.annotation.IODispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class ProductsRepositoryImpl @Inject constructor(
+    private val productsRemoteDataSource: ProductsRemoteDataSource,
+    @IODispatcher private val dispatcher: CoroutineDispatcher,
+) : ProductsRepository {
+    override suspend fun getLatestProducts(): Flow<List<ProductsItem>> =
+        flow { emit(productsRemoteDataSource.getLatestProducts()) }.flowOn(dispatcher)
+
+    override suspend fun getMostProducts(): Flow<List<ProductsItem>> =
+        flow { emit(productsRemoteDataSource.getVisitedProducts()) }.flowOn(dispatcher)
+
+    override suspend fun getTopRatedProducts(page:Int,orderBy:String): Flow<List<ProductsItem>> =
+        flow { emit(productsRemoteDataSource.getTopRatedProducts(page,orderBy)) }.flowOn(dispatcher)
+
+}

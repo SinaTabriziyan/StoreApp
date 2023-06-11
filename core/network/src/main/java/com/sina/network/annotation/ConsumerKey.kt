9458f473af8fdfddf3e84b491c4a6265e8a4789e
sina <cena.pakdel@gmail.com>
2023-06-11 14:42:00 +0330

    modify Consumers.kt class

--- core/network/src/main/java/com/sina/network/annotation/ConsumerKey.kt
+++
@@ -1,7 +0,0 @@
-package com.sina.network.annotation
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class ConsumerKey
--- core/network/src/main/java/com/sina/network/annotation/Consumers.kt
+++ core/network/src/main/java/com/sina/network/annotation/Consumers.kt
@@ -4,4 +4,8 @@ import javax.inject.Qualifier
 
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
+annotation class ConsumerKey
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
 annotation class ConsumerSecret

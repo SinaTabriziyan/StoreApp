150a16958601bf3ccf410d1b5438478ae3d759f7
sina <cena.pakdel@gmail.com>
2023-06-11 14:58:57 +0330

    impl InteractState sealed class

--- domain/domain-main/src/main/java/com/sina/domain_main/usecase/interactor/InteractState.kt
+++ domain/domain-main/src/main/java/com/sina/domain_main/usecase/interactor/InteractState.kt
@@ -4,5 +4,5 @@ package com.sina.domain_main.usecase.interactor
 sealed class InteractState<out T> {
     data class Success<T>(val data: T) : InteractState<T>()
     object Loading : InteractState<Nothing>()
-    object Error : InteractState<Nothing>()
+    data class Error(val errorMessage: String) : InteractState<Nothing>()
 }

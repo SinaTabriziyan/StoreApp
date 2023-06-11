6f51317353ce13fff4a663b99f31e7fb0ed33fb7
sina <cena.pakdel@gmail.com>
2023-06-11 14:55:11 +0330

    impl di DomainModule

---
+++ domain/domain-main/src/main/java/com/sina/domain_main/di/DomainModule.kt
@@ -0,0 +1,16 @@
+package com.sina.domain_main.di
+
+import com.sina.domain_main.repository.ProductsRepository
+import com.sina.domain_main.usecase.interactor.ProductsUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DomainModule {
+
+    @Provides
+    fun provideGetProducts(productsRepository: ProductsRepository): ProductsUseCase = ProductsUseCase(productsRepository)
+}

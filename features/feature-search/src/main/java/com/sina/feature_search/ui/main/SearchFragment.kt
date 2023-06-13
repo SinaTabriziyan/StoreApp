6b34d518316b9fdc4de4ef34bde59384b1fdab4c
sina <cena.pakdel@gmail.com>
2023-06-13 09:22:05 +0330

    fix minor bug paging

--- features/feature-search/src/main/java/com/sina/feature_search/ui/main/SearchFragment.kt
+++ features/feature-search/src/main/java/com/sina/feature_search/ui/main/SearchFragment.kt
@@ -14,7 +14,9 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.sina.feature_search.R
 import com.sina.feature_search.databinding.FragmentSearchBinding
 import com.sina.ui_components.onQueryTextChanged
@@ -38,13 +40,15 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         implMenuToolbar()
         implRecyclerView()
         implObservers()
+        setPagination()
+
     }
 
     private fun implObservers() {
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.productsBySearch.collectLatest {
-                    searchProductsAdapter.submitList(it)
+                    searchProductsAdapter.submitList(it.toMutableList())
                 }
             }
         }
@@ -84,5 +88,17 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
                 return false
             }
         }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun setPagination() {
+        binding.rvSearchProducts.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val lastPosition = layoutManager.findLastVisibleItemPosition()
+                if (lastPosition == searchProductsAdapter.itemCount - 1) {
+                    viewModel.nextPage()
+                }
+            }
+        })
     }
 }
--- features/feature-search/src/main/java/com/sina/feature_search/ui/main/SearchViewModel.kt
+++ features/feature-search/src/main/java/com/sina/feature_search/ui/main/SearchViewModel.kt
@@ -1,11 +1,13 @@
 package com.sina.feature_search.ui.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sina.domain_main.interactor.InteractState
 import com.sina.domain_main.usecase.SearchProductsUseCase
 import com.sina.model.ui.products_item.ProductsItem
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -15,18 +17,28 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(private val searchProductsUseCase: SearchProductsUseCase) : ViewModel() {
-
-    private val _productsBySearch = MutableStateFlow<List<ProductsItem>>(emptyList())
+    private val _productsBySearch = MutableStateFlow<List<ProductsItem>>(mutableListOf())
     val productsBySearch: StateFlow<List<ProductsItem>> = _productsBySearch.asStateFlow()
+    private var job: Job? = null
+    private var page = 1
+    private lateinit var searchQuery: String
+
+    fun nextPage() {
+        page++
+        getProductsBySearch(searchQuery)
+    }
 
     fun getProductsBySearch(searchQuery: String) {
-        viewModelScope.launch {
-            searchProductsUseCase.invoke(SearchProductsUseCase.Params(1, searchQuery)).collectLatest {
+        this.searchQuery = searchQuery
+        job?.cancel()
+        job = viewModelScope.launch {
+            Log.e("TAG", "getProductsBySearch: $page", )
+            searchProductsUseCase.invoke(SearchProductsUseCase.Params(page, searchQuery)).collectLatest {
                 when (it) {
                     is InteractState.Error -> {}
                     is InteractState.Loading -> {}
                     is InteractState.Success -> {
-                        _productsBySearch.value = it.data
+                        _productsBySearch.value += it.data
                     }
                 }
             }
